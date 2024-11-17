8e7b88d94f70f7027e516a95dca5c818955afc30
swathi gowda <swathigowda1035@gmail.com>
2024-11-17 22:43:04 +0530

    Update script.js

--- public/js/script.js
+++ public/js/script.js
@@ -24,7 +24,7 @@ const map = L.map("map").setView([0, 0], 20);
 
 // Load OpenStreetMap tiles
 L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png", {
-    attribution: "Sheryians Coding School",
+    
 }).addTo(map);
 
 // Store markers for each user
